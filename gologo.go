252eedc342185c8ce56eedec164b9e470fe6e9a1
robpatriot <rob@blim.org>
2016-09-15 14:50:00 +0100

    Used Go formatter

--- gologo.go
+++ gologo.go
@@ -6,9 +6,9 @@ import "github.com/AllenDang/w32"
 import (
 	"C"
 	"fmt"
+	"github.com/leedenison/gologo/timer"
 	"syscall"
 	"unsafe"
-	"github.com/leedenison/gologo/timer"
 )
 
 const GRAVITY_VALUE = 5
@@ -19,7 +19,7 @@ const WALL_WIDTH = 10
 const GAP_WIDTH = 80
 
 type shape struct {
-	x, y int32
+	x, y   int32
 	colour uint32
 }
 
@@ -34,7 +34,7 @@ type movableshape struct {
 }
 
 // Feels like the Resistable interface and the movableshape
-// struct are actually the same thing but you can't combine them 
+// struct are actually the same thing but you can't combine them
 // Also this interface isn't actually used yet but theoretically
 // it could be if we had more than one movable object
 type Resistable interface {
@@ -53,30 +53,37 @@ func (s *movableshape) Move() {
 
 func (s *movableshape) ApplyGrav() {
 	s.vx += GRAVITY_VALUE
-	if (s.vx > MAX_SPEED) {s.vx = MAX_SPEED}
+	if s.vx > MAX_SPEED {
+		s.vx = MAX_SPEED
+	}
 }
 
 func (c *circle) ApplyRst() {
 	if c.vy > 0 {
 		c.vy -= c.resistance
-		if c.vy < 0 {c.vy = 0}
+		if c.vy < 0 {
+			c.vy = 0
+		}
 	} else if c.vy < 0 {
 		c.vy += c.resistance
-		if c.vy > 0 {c.vy = 0}
+		if c.vy > 0 {
+			c.vy = 0
+		}
 	}
 }
 
 var ball = circle{}
+
 // TODO Don't know go conventions
 // - structures for the var name seems more sensible but then
 // seems to be trying hard to be confusing
 var walls []structure
 
-func MakeIntResource(id uint16) (*uint16) {
-    return (*uint16)(unsafe.Pointer(uintptr(id)))
+func MakeIntResource(id uint16) *uint16 {
+	return (*uint16)(unsafe.Pointer(uintptr(id)))
 }
 
-func Clamp(value, min, max int32) (int32) {
+func Clamp(value, min, max int32) int32 {
 	switch {
 	case value < min:
 		return min
@@ -86,20 +93,20 @@ func Clamp(value, min, max int32) (int32) {
 	return value
 }
 
-func RGB(r, g, b byte) (uint32) {
-	return (uint32(r) | uint32(g) << 8 | uint32(b) << 16)
+func RGB(r, g, b byte) uint32 {
+	return (uint32(r) | uint32(g)<<8 | uint32(b)<<16)
 }
 
 func CreateObjects() {
 	ball = circle{movableshape: movableshape{
-					shape: shape{
-					  x: 240, y: 30, colour: RGB(0,255,0)},
-					vx: -35, vy: 50},
-				  radius: 20, resistance: CIRCLE_RESISTANCE}
-	
+		shape: shape{
+			x: 240, y: 30, colour: RGB(0, 255, 0)},
+		vx: -35, vy: 50},
+		radius: 20, resistance: CIRCLE_RESISTANCE}
+
 	for i := 0; i < 4; i++ {
 		wall := structure{}
-		wall.colour = RGB(0,0,0)
+		wall.colour = RGB(0, 0, 0)
 		walls = append(walls, wall)
 	}
 }
@@ -117,22 +124,22 @@ func UpdateStructures(hwnd w32.HWND) {
 	walls[1].bottomy = winRect.Right
 	// Top right wall
 	walls[2].y = winRect.Right - WALL_WIDTH
-	walls[2].bottomx = winRect.Bottom / 2 - GAP_WIDTH / 2
+	walls[2].bottomx = winRect.Bottom/2 - GAP_WIDTH/2
 	walls[2].bottomy = winRect.Right
 	// Bottom right wall
-	walls[3].x = winRect.Bottom / 2 + GAP_WIDTH / 2
+	walls[3].x = winRect.Bottom/2 + GAP_WIDTH/2
 	walls[3].y = winRect.Right - WALL_WIDTH
 	walls[3].bottomx = winRect.Bottom
 	walls[3].bottomy = winRect.Right
 }
 
-func Tick(hwnd w32.HWND, uMsg uint32, idEvent uintptr, 
-			dwTime uint16) (uintptr) {
+func Tick(hwnd w32.HWND, uMsg uint32, idEvent uintptr,
+	dwTime uint16) uintptr {
 	// TODO: Need to mutex this so we don't enter twice
 	// Get ball rect
 	ballRect := w32.RECT{Left: ball.y, Top: ball.x,
-						Bottom: ball.x + ball.radius * 2, 
-						Right: ball.y + ball.radius * 2}
+		Bottom: ball.x + ball.radius*2,
+		Right:  ball.y + ball.radius*2}
 	// Clear ball
 	w32.InvalidateRect(hwnd, &ballRect, true)
 
@@ -140,16 +147,16 @@ func Tick(hwnd w32.HWND, uMsg uint32, idEvent uintptr,
 	ball.ApplyGrav()
 	ball.ApplyRst()
 	ball.Move()
-	
+
 	// Check for collisions with walls
 	for i := range walls {
-		closestX := Clamp(ball.x + ball.radius, walls[i].x, walls[i].bottomx)
-		closestY := Clamp(ball.y + ball.radius, walls[i].y, walls[i].bottomy)
+		closestX := Clamp(ball.x+ball.radius, walls[i].x, walls[i].bottomx)
+		closestY := Clamp(ball.y+ball.radius, walls[i].y, walls[i].bottomy)
 		distanceX := ball.x + ball.radius - closestX
 		distanceY := ball.y + ball.radius - closestY
 
-		distanceSqrd := distanceX * distanceX + distanceY * distanceY
-		if distanceSqrd < ball.radius * ball.radius {
+		distanceSqrd := distanceX*distanceX + distanceY*distanceY
+		if distanceSqrd < ball.radius*ball.radius {
 			// hit a wall
 			timer.KillTimer(hwnd, uintptr(TIMER_ID))
 			fmt.Printf("Hit wall %v\n", i)
@@ -158,18 +165,18 @@ func Tick(hwnd w32.HWND, uMsg uint32, idEvent uintptr,
 
 	// Check if we've gone our right
 	winRect := w32.GetClientRect(hwnd)
-	if (ball.y >= winRect.Right) {
+	if ball.y >= winRect.Right {
 		timer.KillTimer(hwnd, uintptr(TIMER_ID))
 		if ball.x < 0 {
 			fmt.Printf("Went over wall\n")
 		} else {
-			fmt.Printf("Win!\n")			
+			fmt.Printf("Win!\n")
 		}
 	}
 
 	// Check if we've hit the left or bottom of the screen
-	if (ball.y + ball.radius * 2 <= 0 ||
-		ball.x >= winRect.Bottom) {
+	if ball.y+ball.radius*2 <= 0 ||
+		ball.x >= winRect.Bottom {
 		timer.KillTimer(hwnd, uintptr(TIMER_ID))
 		fmt.Printf("Went out of play left or down\n")
 	}
@@ -177,97 +184,99 @@ func Tick(hwnd w32.HWND, uMsg uint32, idEvent uintptr,
 	hdc := w32.GetDC(hwnd)
 	PaintMovables(hdc)
 	w32.ReleaseDC(hwnd, hdc)
-	
+
 	return 0
 }
 
 func PaintMovables(hdc w32.HDC) {
 	// Create brush and pen
-    lBrush := w32.LOGBRUSH{LbStyle: w32.BS_SOLID, LbColor: w32.COLORREF(ball.colour)}
-    hBrush := w32.CreateBrushIndirect(&lBrush)
-    hPen := w32.ExtCreatePen(w32.PS_COSMETIC|w32.PS_SOLID, 1, &lBrush, 0, nil)
+	lBrush := w32.LOGBRUSH{LbStyle: w32.BS_SOLID, LbColor: w32.COLORREF(ball.colour)}
+	hBrush := w32.CreateBrushIndirect(&lBrush)
+	hPen := w32.ExtCreatePen(w32.PS_COSMETIC|w32.PS_SOLID, 1, &lBrush, 0, nil)
 
-    // Select pen and store previous
-    previousPen := w32.SelectObject(hdc, w32.HGDIOBJ(hPen))
-    previousBrush := w32.SelectObject(hdc, w32.HGDIOBJ(hBrush))
+	// Select pen and store previous
+	previousPen := w32.SelectObject(hdc, w32.HGDIOBJ(hPen))
+	previousBrush := w32.SelectObject(hdc, w32.HGDIOBJ(hBrush))
 
-    // Draw ball
-    w32.Ellipse(hdc, int(ball.y), int(ball.x), int(ball.y + ball.radius * 2),
-    			 int(ball.x + ball.radius * 2))
+	// Draw ball
+	w32.Ellipse(hdc, int(ball.y), int(ball.x), int(ball.y+ball.radius*2),
+		int(ball.x+ball.radius*2))
 
-    // Delete objects
-    w32.DeleteObject(w32.HGDIOBJ(hPen))
-    w32.DeleteObject(w32.HGDIOBJ(hBrush))
+	// Delete objects
+	w32.DeleteObject(w32.HGDIOBJ(hPen))
+	w32.DeleteObject(w32.HGDIOBJ(hBrush))
 
-    // Reselect previous pen
-    w32.SelectObject(hdc, previousPen)
-    w32.SelectObject(hdc, previousBrush)
+	// Reselect previous pen
+	w32.SelectObject(hdc, previousPen)
+	w32.SelectObject(hdc, previousBrush)
 }
 
-func PaintStructures(hdc w32.HDC) {	
+func PaintStructures(hdc w32.HDC) {
 
-   	if len(walls) == 0 { return }
+	if len(walls) == 0 {
+		return
+	}
 	// Create previous brush and pen holder
-   	var previousPen, previousBrush w32.HGDIOBJ
+	var previousPen, previousBrush w32.HGDIOBJ
 
-    for i := range walls {
+	for i := range walls {
 		// Create brush and pen for this wall
-   		lBrush := w32.LOGBRUSH{LbStyle: w32.BS_SOLID, LbColor: w32.COLORREF(walls[i].colour)}
-   		hBrush := w32.CreateBrushIndirect(&lBrush)
-   		hPen := w32.ExtCreatePen(w32.PS_COSMETIC|w32.PS_SOLID, 1, &lBrush, 0, nil)
+		lBrush := w32.LOGBRUSH{LbStyle: w32.BS_SOLID, LbColor: w32.COLORREF(walls[i].colour)}
+		hBrush := w32.CreateBrushIndirect(&lBrush)
+		hPen := w32.ExtCreatePen(w32.PS_COSMETIC|w32.PS_SOLID, 1, &lBrush, 0, nil)
 
-	    // Select pen
-    	oldPen := w32.SelectObject(hdc, w32.HGDIOBJ(hPen))
-    	oldBrush := w32.SelectObject(hdc, w32.HGDIOBJ(hBrush))
+		// Select pen
+		oldPen := w32.SelectObject(hdc, w32.HGDIOBJ(hPen))
+		oldBrush := w32.SelectObject(hdc, w32.HGDIOBJ(hBrush))
 
-		// TODO Seems hideous to do this but don't know what's better    
-    	if i == 0 {
-    		previousPen = oldPen
-    		previousBrush = oldBrush
-    	}
+		// TODO Seems hideous to do this but don't know what's better
+		if i == 0 {
+			previousPen = oldPen
+			previousBrush = oldBrush
+		}
 
-    	// Draw wall
-    	w32.Rectangle(hdc, int(walls[i].y), int(walls[i].x),
-    				int(walls[i].bottomy), int(walls[i].bottomx))
+		// Draw wall
+		w32.Rectangle(hdc, int(walls[i].y), int(walls[i].x),
+			int(walls[i].bottomy), int(walls[i].bottomx))
 
 		// Delete objects
-   		w32.DeleteObject(w32.HGDIOBJ(hPen))
-    	w32.DeleteObject(w32.HGDIOBJ(hBrush))
-    }
-	
+		w32.DeleteObject(w32.HGDIOBJ(hPen))
+		w32.DeleteObject(w32.HGDIOBJ(hBrush))
+	}
+
 	// Reselect previous pen
-   	w32.SelectObject(hdc, previousPen)		
-   	w32.SelectObject(hdc, previousBrush)
+	w32.SelectObject(hdc, previousPen)
+	w32.SelectObject(hdc, previousBrush)
 }
 
-func WndProc(hwnd w32.HWND, msg uint32, wParam, lParam uintptr) (uintptr) {
+func WndProc(hwnd w32.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	switch msg {
 	case w32.WM_DESTROY:
 		// 0 = WM_QUIT
-        w32.PostQuitMessage(0)
-    case w32.WM_PAINT:
-    	// On initial paint
-    	var ps w32.PAINTSTRUCT
+		w32.PostQuitMessage(0)
+	case w32.WM_PAINT:
+		// On initial paint
+		var ps w32.PAINTSTRUCT
 
-    	hdc := w32.BeginPaint(hwnd, &ps)
-        PaintStructures(hdc)
-        PaintMovables(hdc)
-        w32.EndPaint(hwnd, &ps)
-    case w32.WM_SIZE:
-    	// On resize
-    	var ps w32.PAINTSTRUCT
+		hdc := w32.BeginPaint(hwnd, &ps)
+		PaintStructures(hdc)
+		PaintMovables(hdc)
+		w32.EndPaint(hwnd, &ps)
+	case w32.WM_SIZE:
+		// On resize
+		var ps w32.PAINTSTRUCT
 
-    	hdc := w32.BeginPaint(hwnd, &ps)
-    	UpdateStructures(hwnd)
-    	PaintStructures(hdc)
-    	PaintMovables(hdc)
-    default:
-        return w32.DefWindowProc(hwnd, msg, wParam, lParam)
-    }
-    return 0
+		hdc := w32.BeginPaint(hwnd, &ps)
+		UpdateStructures(hwnd)
+		PaintStructures(hdc)
+		PaintMovables(hdc)
+	default:
+		return w32.DefWindowProc(hwnd, msg, wParam, lParam)
+	}
+	return 0
 }
 
-func CreateWindowClass(hInstance w32.HINSTANCE, lpszClassName *uint16) (w32.WNDCLASSEX) {
+func CreateWindowClass(hInstance w32.HINSTANCE, lpszClassName *uint16) w32.WNDCLASSEX {
 	var wcex w32.WNDCLASSEX
 
 	// Size of the window object.
@@ -318,30 +327,30 @@ func WinMain() int {
 	w32.RegisterClassEx(&wcex)
 
 	// Create an instance of this window class
-	hwnd := w32.CreateWindowEx(0, lpszClassName, 
-					syscall.StringToUTF16Ptr("Simple Go Window!"), 
-					w32.WS_OVERLAPPEDWINDOW | w32.WS_VISIBLE, 
-					w32.CW_USEDEFAULT, w32.CW_USEDEFAULT, 400, 400, 0, 0, 
-					hInstance, nil)
+	hwnd := w32.CreateWindowEx(0, lpszClassName,
+		syscall.StringToUTF16Ptr("Simple Go Window!"),
+		w32.WS_OVERLAPPEDWINDOW|w32.WS_VISIBLE,
+		w32.CW_USEDEFAULT, w32.CW_USEDEFAULT, 400, 400, 0, 0,
+		hInstance, nil)
 
 	w32.ShowWindow(hwnd, w32.SW_SHOWDEFAULT)
 	w32.UpdateWindow(hwnd)
 
 	timer.SetTimer(hwnd, uintptr(TIMER_ID), 100, syscall.NewCallback(Tick))
-   	var msg w32.MSG
-   	for {
-   		// 0, 0, 0 = retrive all messages from all sources
-        if w32.GetMessage(&msg, 0, 0, 0) == 0 {
-            break
-        }
-        w32.TranslateMessage(&msg)
-        w32.DispatchMessage(&msg)
-   	}
-   	return int(msg.WParam)
+	var msg w32.MSG
+	for {
+		// 0, 0, 0 = retrive all messages from all sources
+		if w32.GetMessage(&msg, 0, 0, 0) == 0 {
+			break
+		}
+		w32.TranslateMessage(&msg)
+		w32.DispatchMessage(&msg)
+	}
+	return int(msg.WParam)
 }
 
 func main() {
 	CreateObjects()
-    WinMain()
-    return
+	WinMain()
+	return
 }

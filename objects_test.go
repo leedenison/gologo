b1430edaf2ec586cdfa02f5221d68bd99c016173
Rob Denison <rob@blim.org>
2018-08-16 12:18:53 +0100

    Basic object tests

--- objects_test.go
+++ objects_test.go
@@ -1,24 +1,53 @@
 package gologo
 
 import (
+	"math"
 	"testing"
 
 	"github.com/go-gl/mathgl/mgl32"
 )
 
-// TestObjectCreation : Test basic Object creation by building the object
+// TestSimpleObjectCreation : Test basic Object creation by building the object
 // then reading values back out to confirm values
-func TestObjectCreation(t *testing.T) {
-	/*
-		LoadObjectTemplates("testdata" + pathSeparator + "res")
+// A simple object has no renderer
+func TestSimpleObjectCreation(t *testing.T) {
+	var defaultX, defaultY, translateX, translateY float32 = 500, 400, 150, 100
 
-		objBuilder := Builder()
-		objBuilder.
-			SetPosition(200, 200).
-			Build("BIG_BLUE_SQUARE")
-	*/
-	model := mgl32.Translate3D(400, 400, 0.0).Mul4(mgl32.Ident4().Mul4(mgl32.Ident4()))
-
+	model := mgl32.Translate3D(defaultX, defaultY, 0.0).Mul4(mgl32.Ident4().Mul4(mgl32.Ident4()))
 	obj := CreateObject(model)
 	obj.SetZOrder(1)
+
+	// Test default position
+	x, y := obj.GetPosition()
+	if x != defaultX || y != defaultY {
+		t.Errorf("Location was x (%v), y (%v) should be x (%v), y (%v)",
+			x, y, defaultX, defaultY)
+	}
+
+	// Test translation
+	obj.Translate(translateX, translateY)
+	x, y = obj.GetPosition()
+	if x != defaultX+translateX || y != defaultY+translateY {
+		t.Errorf("After translation location was x (%v), y (%v) should be x (%v), y (%v)",
+			x, y, defaultX+translateX, defaultY+translateY)
+	}
+
+	// Test default direction
+	direction := radToNearestDeg(obj.Direction())
+	if direction%360 != 0 {
+		t.Errorf("Direction was (%v) should be 0", direction)
+	}
+
+	// Test rotating by 90 degrees
+	obj.Rotate(math.Pi / 2)
+	direction = radToNearestDeg(obj.Direction())
+	if direction != 90 {
+		t.Errorf("Direction was (%v) should be 90", direction)
+	}
+
+	// Test object has age
+	age := obj.GetAge()
+	if age < 0 {
+		t.Errorf("Age was (%v) should not be negative", age)
+	}
 }

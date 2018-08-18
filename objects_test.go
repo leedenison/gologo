2ee1e1db8047349d7efdd3504c0adaf6542eec8d
Rob Denison <rob@blim.org>
2018-08-18 22:04:46 +0100

    minor improvements to objects testing

--- objects_test.go
+++ objects_test.go
@@ -1,44 +1,56 @@
 package gologo
 
 import (
-	"math"
 	"testing"
 
 	"github.com/go-gl/mathgl/mgl32"
 )
 
-var (
-	positionX, positionY float32 = 500, 400
-)
+var createObjTests = []struct {
+	posX, posY float32
+	zOrder     int
+}{
+	{200, 300, 1},
+	{0, 0, 2},
+	{-100, -200, 3},
+}
 
 // TestCreateObject : Test basic Object creation by building the object
 // then reading values back out to confirm values
 // A simple object has no renderer
 func TestCreateObject(t *testing.T) {
-	position := mgl32.Vec3{positionX, positionY, 0.0}
-	obj := CreateObject(position)
-	obj.SetZOrder(1)
+	var position mgl32.Vec3
+	var obj *Object
+	var x, y float32
+	var age, direction int
 
-	// Test default position
-	x, y := obj.GetPosition()
-	if x != positionX || y != positionY {
-		t.Errorf("Location was x (%v), y (%v) should be x (%v), y (%v)",
-			x, y, positionX, positionY)
-	}
+	for _, tt := range createObjTests {
+		position = mgl32.Vec3{tt.posX, tt.posY, 0.0}
+		obj = CreateObject(position)
+		obj.SetZOrder(tt.zOrder)
 
-	// Test default direction
-	direction := radToNearestDeg(obj.Direction())
-	if direction%360 != 0 {
-		t.Errorf("Direction was (%v) should be 0", direction)
-	}
+		// Test default position
+		x, y = obj.GetPosition()
+		if x != tt.posX || y != tt.posY {
+			t.Errorf("Location was x (%v), y (%v) should be x (%v), y (%v)",
+				x, y, tt.posX, tt.posY)
+		}
 
-	// Test object has age
-	age := obj.GetAge()
-	if age < 0 {
-		t.Errorf("Age was (%v) should not be negative", age)
+		// Test default direction
+		direction = radToNearestDeg(obj.Direction())
+		if direction%360 != 0 {
+			t.Errorf("Direction was (%v) should be 0", direction)
+		}
+
+		// Test object has age
+		age = obj.GetAge()
+		if age < 0 {
+			t.Errorf("Age was (%v) should not be negative", age)
+		}
 	}
 }
 
+/*
 // TestObject_Translate : Test that Object translation moves the object by the specified amount
 func TestObject_Translate(t *testing.T) {
 	var translateX, translateY float32 = 150, 100
@@ -54,7 +66,6 @@ func TestObject_Translate(t *testing.T) {
 	}
 }
 
-
 // TestObject_Rotate : Test that Object rotation changes the orientation of the object by the specified amount
 func TestObject_Rotate(t *testing.T) {
 	position := mgl32.Vec3{positionX, positionY, 0.0}
@@ -67,138 +78,138 @@ func TestObject_Rotate(t *testing.T) {
 		t.Errorf("Direction was (%v) should be 90", direction)
 	}
 }
-
+*/
 var CasesObject_Integrate = []struct {
-	name string
+	name     string
 	duration float64
-	in  Object
-	out Object
+	in       Object
+	out      Object
 }{
 	{
-		name: "no velocity no forces no damping",
+		name:     "no velocity no forces no damping",
 		duration: 1.0,
 		in: Object{
-			Position: mgl32.Vec3{0, 0, 0},
+			Position:    mgl32.Vec3{0, 0, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 1.0,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{0.0, 0.0, 0.0},
+				InverseMass:     1.0,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{0.0, 0.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{0.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{0.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 		out: Object{
-			Position: mgl32.Vec3{0, 0, 0},
+			Position:    mgl32.Vec3{0, 0, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 1.0,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{0.0, 0.0, 0.0},
+				InverseMass:     1.0,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{0.0, 0.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{0.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{0.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 	},
 	{
-		name: "constant linear velocity no forces no damping",
+		name:     "constant linear velocity no forces no damping",
 		duration: 1.0,
 		in: Object{
-			Position: mgl32.Vec3{0, 0, 0},
+			Position:    mgl32.Vec3{0, 0, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 1.0,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{10.0, 10.0, 0.0},
+				InverseMass:     1.0,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{10.0, 10.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{0.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{0.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 		out: Object{
-			Position: mgl32.Vec3{10, 10, 0},
+			Position:    mgl32.Vec3{10, 10, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 1.0,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{10.0, 10.0, 0.0},
+				InverseMass:     1.0,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{10.0, 10.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{0.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{0.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 	},
 	{
-		name: "deceleration force no damping",
+		name:     "deceleration force no damping",
 		duration: 1.0,
 		in: Object{
-			Position: mgl32.Vec3{0, 0, 0},
+			Position:    mgl32.Vec3{0, 0, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 0.5,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{10.0, 10.0, 0.0},
+				InverseMass:     0.5,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{10.0, 10.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{-10.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{-10.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 		out: Object{
-			Position: mgl32.Vec3{5, 10, 0},
+			Position:    mgl32.Vec3{5, 10, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 0.5,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{5.0, 10.0, 0.0},
+				InverseMass:     0.5,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{5.0, 10.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 1.0,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{-10.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   1.0,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{-10.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 	},
 	{
-		name: "deceleration force 5 percent damping",
+		name:     "deceleration force 5 percent damping",
 		duration: 1.0,
 		in: Object{
-			Position: mgl32.Vec3{0, 0, 0},
+			Position:    mgl32.Vec3{0, 0, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 0.5,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{10.0, 10.0, 0.0},
+				InverseMass:     0.5,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{10.0, 10.0, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 0.95,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{-10.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   0.95,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{-10.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 		out: Object{
-			Position: mgl32.Vec3{4.75, 9.5, 0},
+			Position:    mgl32.Vec3{4.75, 9.5, 0},
 			Orientation: 0,
 			Body: &RigidBody{
-				InverseMass: 0.5,
-				InverseInertia: 1.0,
-				LinearVelocity: mgl32.Vec3{4.75, 9.5, 0.0},
+				InverseMass:     0.5,
+				InverseInertia:  1.0,
+				LinearVelocity:  mgl32.Vec3{4.75, 9.5, 0.0},
 				AngularVelocity: 0.0,
-				LinearDamping: 0.95,
-				AngularDamping: 1.0,
-				Forces: mgl32.Vec3{-10.0, 0.0, 0.0},
-				Torques: 0.0,
+				LinearDamping:   0.95,
+				AngularDamping:  1.0,
+				Forces:          mgl32.Vec3{-10.0, 0.0, 0.0},
+				Torques:         0.0,
 			},
 		},
 	},
@@ -225,10 +236,10 @@ func TestObject_Integrate(t *testing.T) {
 				t.Errorf("Expected post-integration angular velocity: %v, found %v",
 					tc.out.Body.AngularVelocity, tc.in.Body.AngularVelocity)
 			}
-			if (tc.in.Body.InverseMass != tc.out.Body.InverseMass ||
+			if tc.in.Body.InverseMass != tc.out.Body.InverseMass ||
 				tc.in.Body.InverseInertia != tc.out.Body.InverseInertia ||
 				tc.in.Body.LinearDamping != tc.out.Body.LinearDamping ||
-				tc.in.Body.AngularDamping != tc.out.Body.AngularDamping) {
+				tc.in.Body.AngularDamping != tc.out.Body.AngularDamping {
 				t.Errorf("Integration modified rigid body constant - expected: %v, found %v",
 					tc.out.Body, tc.in.Body)
 			}
